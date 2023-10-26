e7e71988681464ecd568e9f8d41a0518088da5c3
Olga Chumachenko <itsistcat@gmail.com>
2023-10-26 03:04:16 +0300

    Пр.14: Готовая работа v.1

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
+const limiter = require('./middlewares/rateLimiter');
+
+const routeSignup = require('./routes/signup');
+const routeSignin = require('./routes/signin');
+
+const auth = require('./middlewares/auth');
+
 const routeUsers = require('./routes/users');
 const routeCards = require('./routes/cards');
-const { ERROR_NOT_FOUND } = require('./errors/errors');
 
-const URL = 'mongodb://0.0.0.0:27017/mestodb';
+const NotFoundError = require('./errors/NotFoundError');
+const errorHandler = require('./middlewares/errorHandler');
+
+const URL = 'mongodb://0.0.0.0.:27017/mestodb';
 const { PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', true);
@@ -13,21 +25,23 @@ mongoose.connect(URL);
 
 const app = express();
 
+app.use(helmet());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '653126017cbe29b70f1fac8d',
-  };
-  next();
-});
+app.use(limiter);
+
+app.use('/', routeSignup);
+app.use('/', routeSignin);
+
+app.use(auth);
 
 app.use('/users', routeUsers);
 app.use('/cards', routeCards);
 
-app.use((req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: 'Страница не найдена' });
-});
+app.use((req, res, next) => next(new NotFoundError('Страницы по запрошенному URL не существует')));
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT);
--- errors/errors.js
+++
@@ -1,9 +0,0 @@
-const ERROR_DATA = 400;
-const ERROR_NOT_FOUND = 404;
-const ERROR_INTERNAL_SERVER = 500;
-
-module.exports = {
-  ERROR_DATA,
-  ERROR_NOT_FOUND,
-  ERROR_INTERNAL_SERVER,
-};
